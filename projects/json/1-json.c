9c86f2bf01234261ea3f6d183e6acc86df4dea82
sjwang <wsj_yx@yeah.net>
2023-11-02 03:30:30 +0000

    json 错误处理

--- projects/json/1-json.c
+++ projects/json/1-json.c
@@ -12,10 +12,16 @@ int main(int argc, char **aggv)
 
     /* 把符合 json 格式的字符串转换成 json 对象 */
     struct json_object *obj = json_tokener_parse(str);
+    if (!obj || is_error(obj))
+    {
+        printf("字符串不是合法的 json 格式\n");
+        exit(EXIT_FAILURE);
+    }
 
     /* json 对象转换为字符串 */
     printf("%s\n", json_object_to_json_string(obj));
 
+    json_object_put(obj);
 
     return 0;
 }
