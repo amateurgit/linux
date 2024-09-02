bbb592b4e6c7e69a21966164e36e4e20a93ed4d0
sjwang <wsj_yx@yeah.net>
2024-09-02 14:13:06 +0800

    线程安全

--- projects/fs/mnt.c
+++ projects/fs/mnt.c
@@ -4,8 +4,6 @@
 
 int main(int argc, char *argv[])
 {
-    struct mntent *mntd = NULL;
-
     FILE *mounts = setmntent("/proc/mounts", "r");
     if ( NULL == mounts )
     {
@@ -15,20 +13,44 @@ int main(int argc, char *argv[])
                 __LINE__);
     }
 
+#if 0
+    struct mntent *mntd = NULL;
+
     while ( (mntd = getmntent(mounts)) )
     {
         printf("#############################\n");
-        printf("文件系统名称 : %s\n"
-                "挂载路径 : %s\n" 
-                "类型 : %s\n" 
+        printf("文件系统 : %s\n"
+                "挂载点 : %s\n" 
+                "文件系统类型 : %s\n" 
                 "选项 : %s\n", 
                 mntd->mnt_fsname,
                 mntd->mnt_dir,
                 mntd->mnt_type,
                 mntd->mnt_opts
-                );
+              );
         printf("#############################\n\n\n");
     }
+#else
+    struct mntent mntd;
+    char buff[1024];
+
+    while ( getmntent_r(mounts, &mntd, buff, sizeof(buff)))
+    {
+        printf("#############################\n");
+        printf("文件系统 : %s\n"
+                "挂载点 : %s\n" 
+                "文件系统类型 : %s\n" 
+                "选项 : %s\n", 
+                mntd.mnt_fsname,
+                mntd.mnt_dir,
+                mntd.mnt_type,
+                mntd.mnt_opts
+              );
+
+        printf("buff = %s\n", buff);
+        printf("#############################\n\n\n");
+    }
+#endif
 
     endmntent( mounts );
 
